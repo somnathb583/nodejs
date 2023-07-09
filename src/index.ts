d3b6b6b749f707c9e3b7fce57f470719624d3bf4
somnathb583 <62545534+somnathb583@users.noreply.github.com>
2023-07-09 15:12:46 +0000

    moved top

--- src/index.ts
+++ src/index.ts
@@ -6,13 +6,13 @@ import { authMiddleware } from "./auth/auth";
 import * as dotenv from 'dotenv';
 
 const app = express();
-app.use(authMiddleware);
-app.use(bodyParser.json());
 app.use((req, res, next) => {
     res.header("Access-Control-Allow-Origin", "*");
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
     next();
 });
+app.use(authMiddleware);
+app.use(bodyParser.json());
 
 dotenv.config();
 
