78448f711d5517b50e8820402f73a9d9e5a1e429
Somnath <somnathb583@gmail.com>
2023-07-15 12:05:34 +0530

    added cors

--- src/index.ts
+++ src/index.ts
@@ -1,21 +1,16 @@
 import { MongoClient } from "mongodb";
 import * as express from 'express';
+import * as cors from 'cors';
 import * as bodyParser from 'body-parser';
 import path = require("path");
 import { authMiddleware } from "./auth/auth";
 import * as dotenv from 'dotenv';
 
 const app = express();
-app.use((req, res, next) => {
-    res.header("Access-Control-Allow-Origin", "*");
-    console.log("it is executed");
-    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-    res.header("Access-Control-Allow-Headers", "access-control-allow-origin");
-    console.log("it is executed also");
-    next();
-});
+
 app.use(authMiddleware);
 app.use(bodyParser.json());
+app.use(cors());
 
 dotenv.config();
 
